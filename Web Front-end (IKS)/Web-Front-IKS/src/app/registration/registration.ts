084ad93f02b497521c9ae21512b9a2cfd3a777d6
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-18 21:22:11 +0100

    Relocate components with fixed routes

--- Web Front-end (IKS)/Web-Front-IKS/src/app/registration/registration.ts
+++ Web Front-end (IKS)/Web-Front-IKS/src/app/registration/registration.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RegistrationForm } from '../registration-form/registration-form';
+import { RegistrationForm } from './registration-form/registration-form';
 
 @Component({
   selector: 'app-registration',
