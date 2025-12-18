950e5fc2a0787fce73392e585c29801d0bdca886
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-18 04:07:13 +0100

    Add forgot-password and reset-password

--- Web Front-end (IKS)/Web-Front-IKS/src/app/app.routes.ts
+++ Web Front-end (IKS)/Web-Front-IKS/src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Registration } from './registration/registration';
 import { Login } from './login/login';
+import { ForgotPassword } from './forgot-password/forgot-password';
+import { ResetPassword } from './reset-password/reset-password';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register', component: Registration },
   { path: 'login', component: Login },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'reset-password', component: ResetPassword },
 ];
