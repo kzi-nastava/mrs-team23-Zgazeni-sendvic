ede076d19269d5b9bfe1bfb14084ca438df90a77
Marko <marko28082004@gmail.com>
2025-12-19 17:45:29 +0100

    feat: initialize Spring Boot application with necessary configurations and files.

---
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/ServerBackIssApplication.java
@@ -0,0 +1,13 @@
+package ZgazeniSendvic.Server_Back_ISS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBackIssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBackIssApplication.class, args);
+	}
+
+}
