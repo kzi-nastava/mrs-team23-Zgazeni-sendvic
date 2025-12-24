4a473c5cf9dc022ed837899af0cca312117eeb05
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-24 20:53:01 +0100

    Finish controller for routes

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controllers/RideController.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controllers/RideController.java
@@ -1,10 +1,29 @@
 package ZgazeniSendvic.Server_Back_ISS.controllers;
 
+import ZgazeniSendvic.Server_Back_ISS.entity.RideRoute;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 class RideController {
+    @GetMapping("ride-estimation/{arrival}/{destinationsStr}")
+    public ResponseEntity<RideRoute> estimateRide(@PathVariable String arrival, @PathVariable String destinationsStr){
+        List<String> destinations = new ArrayList<>(Arrays.asList(destinationsStr.split(",")));
+        RideRoute route = new RideRoute(destinations);
 
+        
+
+        return new ResponseEntity<RideRoute>(route, HttpStatus.OK);
+
+
+    }
 }
