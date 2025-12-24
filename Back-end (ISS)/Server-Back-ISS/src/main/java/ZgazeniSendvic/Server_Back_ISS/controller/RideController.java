51cee908334a30c00dcc97dfaa0b83676aa95faf
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-25 00:24:53 +0100

    Add ride cancelling

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controller/RideController.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controller/RideController.java
@@ -1,13 +1,13 @@
 package ZgazeniSendvic.Server_Back_ISS.controller;
 
+import ZgazeniSendvic.Server_Back_ISS.dto.DriveCancelDTO;
+import ZgazeniSendvic.Server_Back_ISS.dto.DriveCancelledDTO;
 import ZgazeniSendvic.Server_Back_ISS.dto.RouteEstimationDTO;
 import ZgazeniSendvic.Server_Back_ISS.entity.RideRoute;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,4 +28,23 @@ class RideController {
 
 
     }
+
+    @PutMapping(path = "ride-tracking",
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DriveCancelledDTO> cancelDrive(@RequestBody DriveCancelDTO cancelRequest) throws Exception{
+
+        DriveCancelledDTO cancelled = new DriveCancelledDTO();
+        cancelled.setReason(cancelRequest.getReason());
+        cancelled.setRideID(cancelRequest.getRideID());
+        cancelled.setTime(cancelRequest.getTime());
+        cancelled.setRequesterID(cancelRequest.getRequesterID());
+
+        //process that would decide wether to or not to
+        cancelled.setCancelled(true);
+
+        return new ResponseEntity<DriveCancelledDTO>(cancelled, HttpStatus.OK);
+
+
+    }
+
 }
