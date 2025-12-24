6f19241c37a05bb17eaa130c071f212080ece7b6
Marko <marko28082004@gmail.com>
2025-12-24 01:11:05 +0100

    Added java classes for the registration of drivers, through the DriverController.

---
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/dto/CreatedDriverDTO.java
@@ -0,0 +1,10 @@
+package ZgazeniSendvic.Server_Back_ISS.dto;
+
+import ZgazeniSendvic.Server_Back_ISS.model.Account;
+import ZgazeniSendvic.Server_Back_ISS.model.Vehicle;
+
+public class CreatedDriverDTO {
+    private Long id;
+    private Account account;
+    private Vehicle vehicle;
+}
---
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/model/Account.java
@@ -0,0 +1,9 @@
+package ZgazeniSendvic.Server_Back_ISS.model;
+
+public class Account {
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
