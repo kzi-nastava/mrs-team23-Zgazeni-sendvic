0bae4125a5cd3580bebcfa32f4e6c36e42126a76
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-25 23:20:05 +0100

    Add dummy objects for password reset

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/dto/PasswordResetRequestDTO.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/dto/PasswordResetRequestDTO.java
@@ -1,5 +1,17 @@
 package ZgazeniSendvic.Server_Back_ISS.dto;
 
 public class PasswordResetRequestDTO {
-    
+    private String Email;
+
+    public PasswordResetRequestDTO(String email) {
+        Email = email;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
 }
