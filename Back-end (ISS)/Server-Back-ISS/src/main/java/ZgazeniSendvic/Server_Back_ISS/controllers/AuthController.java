75776ee1bd152ce9d2f52452c3be8cd3b70ecd4e
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-25 22:46:50 +0100

    Update register endpoint

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controllers/AuthController.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controllers/AuthController.java
@@ -1,21 +1,31 @@
 package ZgazeniSendvic.Server_Back_ISS.controllers;
 
+import ZgazeniSendvic.Server_Back_ISS.dto.LoginRequestedDTO;
+import ZgazeniSendvic.Server_Back_ISS.dto.RegisterRequestDTO;
+import ZgazeniSendvic.Server_Back_ISS.dto.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 class AuthController {
 
-    @GetMapping("register")
-    public String getRegister(){
-        /* Would send the Register page, so that the client can load */
-        return "Register Page requested Successfully";
-    }
 
-    @PostMapping("register")
-    public String register(@RequestBody String body){
+    @PostMapping(path = "register", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<LoginRequestedDTO> register(@RequestBody RegisterRequestDTO body) throws Exception{
 
-        return "Register info sent successfully";
+        //would check wether creation is acceptable here
+        //successful
+        UserDTO userDto = new UserDTO(body.getEmail(), "82340248SAsdad", body.getFirstName(), body.getLastName(),
+                body.getAddress(), body.getPhoneNum(), body.getPictUrl());
+
+        //login immediately
+        LoginRequestedDTO loginDTO = new LoginRequestedDTO("3424asd", "bearer", userDto);
+
+        return new ResponseEntity<LoginRequestedDTO>(loginDTO, HttpStatus.CREATED);
 
     }
 
