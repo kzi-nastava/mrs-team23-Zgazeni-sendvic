36bc9ee01ef8cfde1d4a6073cc1d51c09c559e45
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-25 22:58:27 +0100

    Clean up for merge

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controllers/AuthController.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controllers/AuthController.java
@@ -6,38 +6,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 class AuthController {
 
-    @GetMapping("register")
-    public String getRegister(){
-        /* Would send the Register page, so that the client can load */
-        return "Register Page requested Successfully";
-    }
-
-    @PostMapping("register")
-    public String register(@RequestBody String body){
-
-        return "Register info sent successfully \n" + body;
-
-    }
-
-    @GetMapping("login")
-    public String getLogin(){
-        return "Login Page requested Successfully";
-    }
-
-    @PostMapping("login")
-    public String login(@RequestBody String body){
-        return "Login info sent successfully \n" + body;
-    }
-
-    @GetMapping("forgotten-password/{email}")
-    public String forgotPassword(@PathVariable String email){
-        return "Sent Email: " + email;
-    }
-
-    @PostMapping("reset-password")
-    public String resetPassword(@RequestBody String body){
-
-        return "resetPass info sent!";
-    }
+    
 
 }
