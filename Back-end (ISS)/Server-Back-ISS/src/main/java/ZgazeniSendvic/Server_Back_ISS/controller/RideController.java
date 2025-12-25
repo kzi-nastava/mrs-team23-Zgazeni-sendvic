b427dc8aa2dd2e7982524a4c9646741f5bd9424c
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2025-12-25 20:20:11 +0100

    add retrieving rides by admin

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controller/RideController.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controller/RideController.java
@@ -1,17 +1,19 @@
 package ZgazeniSendvic.Server_Back_ISS.controller;
 
+import ZgazeniSendvic.Server_Back_ISS.dto.ARideRequestedDTO;
 import ZgazeniSendvic.Server_Back_ISS.dto.RideStopDTO;
 import ZgazeniSendvic.Server_Back_ISS.dto.RideStoppedDTO;
 import ZgazeniSendvic.Server_Back_ISS.dto.RouteEstimationDTO;
 import ZgazeniSendvic.Server_Back_ISS.entity.RideRoute;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.awt.print.Pageable;
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
 @RequestMapping("/")
@@ -50,6 +52,36 @@ class RideController {
         return new ResponseEntity<RideStoppedDTO>(stopped, HttpStatus.OK);
 
     }
+
+    @GetMapping(path = "admin-HOR/{targedID}",consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ARideRequestedDTO>> adminRetrieveRides
+            (@PathVariable String targetID,
+            Pageable pageable,
+             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate)
+    throws Exception{
+        // here a service would go over the pageable and request params etc...
+
+        ARideRequestedDTO ride = new ARideRequestedDTO(
+                UUID.randomUUID().toString(),                   //rideID
+                Arrays.asList("Stop A", "Stop B", "Stop C"),   //destinations
+                "Start Stop",
+                new Date(),                                     //beginning
+                new Date(System.currentTimeMillis() + 3600000),//ending (1 hour later)
+                false,                                          //wasCancelled
+                null,                                           //whoCancelled
+                29.99,                                          //price
+                false                                           //panic
+        );
+
+        List<ARideRequestedDTO> allRides = new ArrayList<>();
+        allRides.add(ride);
+
+        return new ResponseEntity<List<ARideRequestedDTO>>(allRides, HttpStatus.OK);
+
+    }
+
 
 
 
