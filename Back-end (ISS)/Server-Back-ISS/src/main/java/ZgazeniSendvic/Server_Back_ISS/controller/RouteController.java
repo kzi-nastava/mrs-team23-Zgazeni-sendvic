dfd35be1c81bf25f4fbb9787a33bd76e2fdfd870
Marko <marko28082004@gmail.com>
2025-12-28 23:15:01 +0100

    Adjusted implementation of RouteController to correctly return all of a users favourite routes.

--- Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controller/RouteController.java
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/controller/RouteController.java
@@ -10,18 +10,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/route")
 public class RouteController {
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GetRouteDTO> getRoute(@PathVariable("id") Long id) {
-        GetRouteDTO route = new GetRouteDTO();
+    @GetMapping(path = "/favorites", value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<GetRouteDTO>> getFavoriteRoutes(@PathVariable("id") Long id) {
+        Collection<GetRouteDTO> routes = new ArrayList<>();
+        GetRouteDTO route1 = new GetRouteDTO();
+        GetRouteDTO route2 = new GetRouteDTO();
 
-        route.setId(1L);
-        route.setStart(new Location(1L, 50.0, 50.0));
-        route.setDestination(new Location(1L, 50.0, 50.0));
+        route1.setId(1L);
+        route1.setStart(new Location(1L, 50.0, 50.0));
+        route1.setDestination(new Location(1L, 50.0, 50.0));
 
-        return new ResponseEntity<GetRouteDTO>(route, HttpStatus.OK);
+        route2.setId(1L);
+        route2.setStart(new Location(1L, 50.0, 50.0));
+        route2.setDestination(new Location(1L, 50.0, 50.0));
+
+        routes.add(route1);
+        routes.add(route2);
+
+        return new ResponseEntity<Collection<GetRouteDTO>>(routes, HttpStatus.OK);
     }
 }
