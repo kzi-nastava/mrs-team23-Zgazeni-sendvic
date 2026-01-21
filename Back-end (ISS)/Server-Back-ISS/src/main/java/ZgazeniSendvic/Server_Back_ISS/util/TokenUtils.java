4e9ff3027b5085e5bb3ed1853a597b77d110fd83
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2026-01-21 20:32:49 +0100

    Add basic TokenUtils

---
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/util/TokenUtils.java
@@ -0,0 +1,215 @@
+package ZgazeniSendvic.Server_Back_ISS.util;
+
+import ZgazeniSendvic.Server_Back_ISS.model.Account;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class TokenUtils {
+
+    // Token issuer
+    @Value("DriveBy")
+    private String APP_NAME;
+
+    // Secret
+    @Value("Sendic")
+    public String SECRET;
+
+    // Validity time - 30 min
+    @Value("1800000")
+    private int EXPIRES_IN;
+
+    // Header through which JWT is passed in client-server communication
+    @Value("Authorization")
+    private String AUTH_HEADER;
+
+    private static final String AUDIENCE_WEB = "web";
+
+    // Algorithm for JWT signing
+    private SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS512;
+
+
+    // ============= JWT generation =============
+
+    //Gen of the token, contains the email for ID-ing
+    public String generateToken(Account account) {
+        return Jwts.builder()
+                .setIssuer(APP_NAME)
+                .setSubject(account.getEmail())
+                .setAudience(generateAudience())
+                .setIssuedAt(new Date())
+                .claim("roles", account.getRoles().toString())
+                .setExpiration(generateExpirationDate())
+                .signWith(SIGNATURE_ALGORITHM, SECRET).compact();
+
+
+        // Roles are the only important info right?
+    }
+
+    //would determine weather pc/web etc., for now only web
+    private String generateAudience() {
+
+
+        return AUDIENCE_WEB;
+    }
+
+    //simply generates up to which point the token is valid
+    private Date generateExpirationDate() {
+        return new Date(new Date().getTime() + EXPIRES_IN);
+    }
+
+    // ============= Reading from JWT =============
+
+    //gets the token from the header
+    public String getToken(HttpServletRequest request) {
+        String authHeader = getAuthHeaderFromHeader(request);
+        // JWT se is passed through header 'Authorization' in format:
+        // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7); // only the token (value after "Bearer " prefix)
+        }
+
+        return null;
+    }
+
+    //func for obtaining the email from the Token
+    //returns null if an exception, moves expiry exception up.
+    public String getUsernameFromToken(String token) {
+        String email;
+
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            email = claims.getSubject();
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        } catch (Exception e) {
+            email = null;
+        }
+
+        return email;
+    }
+
+    //returns date of creation
+    public Date getIssuedAtDateFromToken(String token) {
+        Date issueAt;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            issueAt = claims.getIssuedAt();
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        } catch (Exception e) {
+            issueAt = null;
+        }
+        return issueAt;
+    }
+
+    //returns audience info
+    public String getAudienceFromToken(String token) {
+        String audience;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            audience = claims.getAudience();
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        } catch (Exception e) {
+            audience = null;
+        }
+        return audience;
+    }
+
+    //returns expiry date
+    public Date getExpirationDateFromToken(String token) {
+        Date expiration;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            expiration = claims.getExpiration();
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        } catch (Exception e) {
+            expiration = null;
+        }
+
+        return expiration;
+    }
+
+    // returns all claims
+    private Claims getAllClaimsFromToken(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException ex) {
+            throw ex;
+        } catch (Exception e) {
+            claims = null;
+        }
+
+        // Obtaining extra info : claims.get(key)
+
+        return claims;
+    }
+
+    // =================================================================
+
+    // ============= Functions for validation of JWT =============
+
+    /**
+     * Validation of the JWT.
+     *
+     * @param token JWT token.
+     * @param userDetails Info about the Owner of the token.
+     * @return Information on validity of the Token.
+     * not sure exactly how one part functions, will come back to it
+     */
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        Account account = (Account) userDetails;
+        final String email = getUsernameFromToken(token);
+        final Date created = getIssuedAtDateFromToken(token);
+
+        // Token je validan kada:
+        return (email != null // korisnicko ime nije null
+                && email.equals(userDetails.getUsername())) ;// Token name is equal to userDetails name
+                //would be password-reset date check, for now nothing);
+    }
+    /**
+     * Funkcija proverava da li je lozinka korisnika izmenjena nakon izdavanja tokena.
+     *
+     * @param created Date of token creation.
+     * @param lastPasswordReset Date of last password reset.
+     * @return Was token created before or after the last password reset.
+     */
+    private Boolean isCreatedBeforeLastPasswordReset(Date created, Date lastPasswordReset) {
+        return (lastPasswordReset != null && created.before(lastPasswordReset));
+    }
+
+    // =================================================================
+
+    // gets the time of expiry
+    public int getExpiredIn() {
+        return EXPIRES_IN;
+    }
+
+    /**
+     * Function for obtaining Auth header
+     *
+     * @param request HTTP request.
+     *
+     * @return content of AUTH_HEADER.
+     */
+    public String getAuthHeaderFromHeader(HttpServletRequest request) {
+        return request.getHeader(AUTH_HEADER);
+    }
+
+}
+}
