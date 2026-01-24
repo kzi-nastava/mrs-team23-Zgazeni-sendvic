2fefa21fe86f98444391a55537156a85ed54aa3c
Stefan Nalcic <nalcic.sv64.2023@uns.ac.rs>
2026-01-24 20:41:05 +0100

    Add basic JWT necessities

---
+++ Back-end (ISS)/Server-Back-ISS/src/main/java/ZgazeniSendvic/Server_Back_ISS/security/auth/RestAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package ZgazeniSendvic.Server_Back_ISS.security.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    //when something within the Security filter chain fails, it eventually ends up here. Failure within the chain
+    //implies lack of AUTH, thus the response
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
